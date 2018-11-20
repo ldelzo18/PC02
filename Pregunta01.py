04c3d4550583b3464115d6a1a5b8209d8042211b
Luis Delzo E <ldelzo.e@gmail.com>
2018-11-20 07:30:38 -0500

    Pregunta 01

---
+++ Pregunta01.py
@@ -0,0 +1,9 @@
+import math
+
+x = (float)(input("Ingrese la coordenada x:\n"))
+y = (float)(input("Ingrese la coordenada y:\n"))
+
+rs = math.sqrt((x*x)+(y*y))
+print("La distancia entre el punto ingresado y el origen de coordenadas es: ",rs)
+print("La coodenada ingresada es x:",x,", y:",y)
+    
